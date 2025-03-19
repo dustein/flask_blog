5fde2fe3ab9d5720e47fe8c955f3368e95c50961
dustein <steindu@gmail.com>
2025-03-19 16:27:43 -0300

    salvar funciona

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+# url_for('static', filename='style.css')
+
+@app.route("/")
+def nova_pagina():
+    site_estatico = render_template("index.html")
+    converte_estatico(site_estatico, "primeiro-teste.html")
+    return render_template("index.html")
+
+def converte_estatico(renderizado, nova_pagina):
+    output_dir = 'estaticos'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+
+
+    file_path = os.path.join(output_dir, nova_pagina)
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(renderizado)
+
+app.run(debug=True)
